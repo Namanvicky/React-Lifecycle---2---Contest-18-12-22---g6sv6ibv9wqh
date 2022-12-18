7aa6c0b65c7f60edcd7e6c1e3949fb8fa40fa725
Namanvicky <116458366+Namanvicky@users.noreply.github.com>
2022-12-18 18:39:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,26 @@
-import React from 'react'
-
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+class App extends Component {
+    constructor(props) {
+		super(props);
+	    	this.state={display:false};
+		this.handleClick = this.handleClick.bind(this)
+	};
+	
+handleClick(){
+ 	this.setState({display:true}); 
+}
 
-class App extends React.Component {
-
-  handleClick(){
-    this.setState({count: this.state.count+1})
-  }
-  constructor(props){
-    super(props)
-    this.state = {count:0}
-    this.handleClick = this.handleClick.bind(this);
-
-  }
-
-
-  
-  render() {
-    console.log('rendered')
-    return (
-      <div id="main">
-          <div id="count">{this.state.count}</div>
-          <button id="incr" onClick={this.handleClick}>Increment</button>
-      </div>
-    )
-  }
+render() {
+	return(
+    		<div id="main">
+			{ /* Do not remove this main div!! */ }
+			<button id='click' onClick={this.handleClick} >Click</button>
+			{this.state.display===true?<p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:''}
+    		</div>
+    	);
+    }
 }
 
 
